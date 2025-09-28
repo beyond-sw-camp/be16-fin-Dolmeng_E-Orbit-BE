a2f120e15c3ec0ac73665c79047087bed2b63591
EunDuk2 <dmsqls6789@naver.com>
2025-09-28 23:30:11 +0900

    Feat: 회원가입 구현_4 - 프로필 이미지 업로드를 위해 S3 의존성 추가 - S3 설정 및 파일 업로드 모듈 추가 (AwsS3Config, S3Uploader)

--- user/src/main/java/com/Dolmeng_E/user/app/service/UserService.java
+++ user/src/main/java/com/Dolmeng_E/user/app/service/UserService.java
@@ -3,9 +3,8 @@ package com.Dolmeng_E.user.app.service;
 import com.Dolmeng_E.user.app.domain.User;
 import com.Dolmeng_E.user.app.dto.UserCreateResDto;
 import com.Dolmeng_E.user.app.repository.UserRepository;
+import com.Dolmeng_E.user.common.service.S3Uploader;
 import jakarta.persistence.EntityExistsException;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,14 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final S3Uploader s3Uploader;
 
     public void create(UserCreateResDto dto) {
         if(userRepository.findByEmail(dto.getEmail()).isPresent()) throw new EntityExistsException("중복되는 이메일입니다.");
 
         // todo - 비밀번호 암호화, 프로필 이미지 저장 후 url 가져오기
         String encodedPassword = passwordEncoder.encode(dto.getPassword());
+        String profileImgaeUrl = s3Uploader.upload(dto.getProfileImageUrl(), "user");
 
-        User user = dto.toEntity(encodedPassword, "url");
+        User user = dto.toEntity(encodedPassword, profileImgaeUrl);
         userRepository.save(user);
     }
 
