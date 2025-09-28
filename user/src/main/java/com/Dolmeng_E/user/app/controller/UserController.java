30c2dd5a4acfce47295ba69e4e6aa2f74a9dd90b
EunDuk2 <dmsqls6789@naver.com>
2025-09-29 00:19:33 +0900

    Feat: 로그인 구현_2 - 로그인 API 구현 - Controller, Service 코드 구현 - 경로: /user/login

--- user/src/main/java/com/Dolmeng_E/user/app/controller/UserController.java
+++ user/src/main/java/com/Dolmeng_E/user/app/controller/UserController.java
@@ -1,25 +1,37 @@
 package com.Dolmeng_E.user.app.controller;
 
-import com.Dolmeng_E.user.app.dto.UserCreateResDto;
+import com.Dolmeng_E.user.app.domain.User;
+import com.Dolmeng_E.user.app.dto.UserCreateReqDto;
+import com.Dolmeng_E.user.app.dto.UserLoginReqDto;
+import com.Dolmeng_E.user.app.dto.UserLoginResDto;
 import com.Dolmeng_E.user.app.service.UserService;
+import com.Dolmeng_E.user.common.auth.JwtTokenProvider;
 import com.Dolmeng_E.user.common.dto.CommonSuccessDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/create")
-    public ResponseEntity<?> create(@ModelAttribute UserCreateResDto dto) {
+    public ResponseEntity<?> create(@ModelAttribute @Valid UserCreateReqDto dto) {
         userService.create(dto);
         return new ResponseEntity<>(new CommonSuccessDto(dto.getEmail(), HttpStatus.CREATED.value(), "회원가입 성공"), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid UserLoginReqDto dto) {
+        User user = userService.login(dto);
+
+        String accessToken = jwtTokenProvider.createAtToken(user);
+        String refreshToken = jwtTokenProvider.createRtToken(user);
+        return new ResponseEntity<>(new CommonSuccessDto(new UserLoginResDto(accessToken, refreshToken), HttpStatus.OK.value(), "로그인 성공"), HttpStatus.OK);
     }
 }
--- user/src/main/java/com/Dolmeng_E/user/app/service/UserService.java
+++ user/src/main/java/com/Dolmeng_E/user/app/service/UserService.java
@@ -1,10 +1,12 @@
 package com.Dolmeng_E.user.app.service;
 
 import com.Dolmeng_E.user.app.domain.User;
-import com.Dolmeng_E.user.app.dto.UserCreateResDto;
+import com.Dolmeng_E.user.app.dto.UserCreateReqDto;
+import com.Dolmeng_E.user.app.dto.UserLoginReqDto;
 import com.Dolmeng_E.user.app.repository.UserRepository;
 import com.Dolmeng_E.user.common.service.S3Uploader;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,15 +20,20 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final S3Uploader s3Uploader;
 
-    public void create(UserCreateResDto dto) {
+    public void create(UserCreateReqDto dto) {
         if(userRepository.findByEmail(dto.getEmail()).isPresent()) throw new EntityExistsException("중복되는 이메일입니다.");
 
-        // todo - 비밀번호 암호화, 프로필 이미지 저장 후 url 가져오기
         String encodedPassword = passwordEncoder.encode(dto.getPassword());
         String profileImgaeUrl = s3Uploader.upload(dto.getProfileImageUrl(), "user");
 
         User user = dto.toEntity(encodedPassword, profileImgaeUrl);
         userRepository.save(user);
+    }
+
+    public User login(UserLoginReqDto dto) {
+        User user = userRepository.findByEmail(dto.getEmail()).orElseThrow(() -> new EntityNotFoundException("없는 회원입니다."));
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())) throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
+        return user;
     }
 
 }
