0dcb177a9964b174efe0a0a2519aa1936dd9ca73
EunDuk2 <dmsqls6789@naver.com>
2025-10-04 00:14:54 +0900

    Feat: google oauth로그인 구현 - /user/google/login 경로 화이트리스트 추가 - google로그인 API 구현

--- api-gateway/src/main/java/com/Dolmeng_E/api_gateway/JwtAuthFilter.java
+++ api-gateway/src/main/java/com/Dolmeng_E/api_gateway/JwtAuthFilter.java
@@ -24,6 +24,7 @@ public class JwtAuthFilter implements GlobalFilter {
             "/user/auth/login",
             "/user/auth/refresh",
             "/user/kakao/login",
+            "/user/google/login",
             "/health"
     );
 
