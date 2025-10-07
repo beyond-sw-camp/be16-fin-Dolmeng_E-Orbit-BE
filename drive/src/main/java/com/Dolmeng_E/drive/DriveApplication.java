df0a844645ea7ca1b5df374fd17454edc8c21a60
YoungKwanK <young.kwin@gmail.com>
2025-10-07 20:28:14 +0900

    Refactor: 공통 모듈 도입으로 인한 코드 리팩토링 - 공통 모듈을 도입하여 중복 코드를 제거하고, 코드의 가독성을 높였습니다.

--- drive/src/main/java/com/Dolmeng_E/drive/DriveApplication.java
+++ drive/src/main/java/com/Dolmeng_E/drive/DriveApplication.java
@@ -3,9 +3,14 @@ package com.Dolmeng_E.drive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.Dolmeng_E.drive",
+		"com.example.modulecommon"
+})
 public class DriveApplication {
 
 	public static void main(String[] args) {
