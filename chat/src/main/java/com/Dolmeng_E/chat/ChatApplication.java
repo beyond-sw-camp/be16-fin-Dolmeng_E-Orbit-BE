bd3db79e72bed5f6cec4abfc215c0125adcb0b61
EunDuk2 <dmsqls6789@naver.com>
2025-10-09 00:41:38 +0900

    Chore: 최초 개발 전 init 세팅

--- chat/src/main/java/com/Dolmeng_E/chat/ChatApplication.java
+++ chat/src/main/java/com/Dolmeng_E/chat/ChatApplication.java
@@ -3,9 +3,14 @@ package com.Dolmeng_E.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.Dolmeng_E.chat",
+        "com.example.modulecommon"
+})
 public class ChatApplication {
 
 	public static void main(String[] args) {
