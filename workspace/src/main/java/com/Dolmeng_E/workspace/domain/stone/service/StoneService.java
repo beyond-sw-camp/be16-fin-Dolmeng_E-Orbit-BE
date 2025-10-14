317c9e5cf2c6488e18a38034400f22cc09171d37
jominhyeong97 <jumbo0303@daum.net>
2025-10-14 20:27:34 +0900

    Feat: 구현 로직 구상

--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/service/StoneService.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/service/StoneService.java
@@ -14,4 +14,28 @@ public class StoneService {
     private final ChildStoneListRepository childStoneListRepository;
     private final TaskRepository taskRepository;
     private final StoneRepository stoneRepository;
+
+
+    // 스톤 생성
+
+    // 스톤 수정
+
+    // 스톤 삭제
+
+    // 스톤 목록? 스톤들 뿌리처럼 보이는 거
+
+    // 스톤 상세 정보 조회
+
+    // 태스크 생성
+
+    // 태스크 수정
+
+    // 태스크 삭제
+
+    // 태스크 완료 처리
+
+    // 마일스톤 진행률 변경
+
+    // To-Do: 다 하면 프로젝트 쪽 로직 완성
+
 }
