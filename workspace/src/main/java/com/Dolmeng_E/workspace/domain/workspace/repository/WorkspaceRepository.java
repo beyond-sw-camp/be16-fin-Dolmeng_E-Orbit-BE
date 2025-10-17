41b8ff815a58b5ae7bf6c8fea8e94b2e18f2b973
ifunhy <guswl8296@gmail.com>
2025-10-17 18:51:13 +0900

    Feat: 워크스페이스 존재 여부 repository 확인

--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/workspace/repository/WorkspaceRepository.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/workspace/repository/WorkspaceRepository.java
@@ -9,5 +9,9 @@ import java.util.UUID;
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, String> {
     Optional<Workspace> findByIdAndIsDeleteFalse(String workspaceId);
+
     Workspace findByUserIdAndWorkspaceName(UUID userId, String workspaceName);
+
+    // 워크스페이스 존재 여부 확인
+    boolean existsById(String workspaceId);
 }
