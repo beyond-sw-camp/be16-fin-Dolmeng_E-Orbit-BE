96d892d2f1ffc69c04fcadd7fbceae3419732af5
ifunhy <guswl8296@gmail.com>
2025-10-20 17:56:54 +0900

    Chore: startedAt 컬럼명 수정 및 calendarType 수정

--- user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/dto/CreateScheduleReqDto.java
+++ user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/dto/CreateScheduleReqDto.java
@@ -1,16 +1,21 @@
 package com.Dolmeng_E.user.domain.sharedCalendar.dto;
 
 import com.Dolmeng_E.user.domain.sharedCalendar.entity.CalendarType;
+import lombok.Builder;
 import lombok.Getter;
+import com.Dolmeng_E.user.domain.sharedCalendar.entity.CalendarType;
+
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class CreateScheduleReqDto {
     private String workspaceId;
     private String calendarName;
-    private LocalDateTime startAt;
+    private LocalDateTime startedAt;
     private LocalDateTime endedAt;
     private Boolean isShared;
-    private CalendarType CalendarType;
+    @Builder.Default
+    private CalendarType calendarType = CalendarType.SCHEDULE;
 }
