10ee705a9dfa12e83d763967f45995fbe148a8e3
ifunhy <guswl8296@gmail.com>
2025-10-21 11:38:10 +0900

    Feat: todo 수정 로직 구현 및 todo 등록 시 날짜 변환 로직 구현

--- user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/service/TodoService.java
+++ user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/service/TodoService.java
@@ -2,12 +2,14 @@ package com.Dolmeng_E.user.domain.sharedCalendar.service;
 
 import com.Dolmeng_E.user.domain.sharedCalendar.dto.TodoCreateReqDto;
 import com.Dolmeng_E.user.domain.sharedCalendar.dto.TodoCreateResDto;
+import com.Dolmeng_E.user.domain.sharedCalendar.dto.UpdateTodoReqDto;
 import com.Dolmeng_E.user.domain.sharedCalendar.entity.CalendarType;
 import com.Dolmeng_E.user.domain.sharedCalendar.entity.SharedCalendar;
 import com.Dolmeng_E.user.domain.sharedCalendar.repository.SharedCalendarRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,14 +27,18 @@ public class TodoService {
         // 1. 검증
         var user = validationService.validateUserAndWorkspace(userId, dto.getWorkspaceId());
 
-        // 2. todo 생성
+        // 2. 날짜 변환: 하루 전체로 설정
+        LocalDateTime startedAt = dto.getDate().atStartOfDay();              // 2025-10-20T00:00:00
+        LocalDateTime endedAt = dto.getDate().atTime(23, 59, 59);            // 2025-10-20T23:59:59
+
+        // 3. todo 생성
         SharedCalendar todo = SharedCalendar.builder()
                 .userId(user)
                 .workspaceId(dto.getWorkspaceId())
                 .calendarType(dto.getCalendarType())
                 .calendarName(dto.getCalendarName())
-                .startedAt(dto.getStartedAt())
-                .endedAt(dto.getEndedAt())
+                .startedAt(startedAt)
+                .endedAt(endedAt)
                 .bookmark(dto.getBookmark())
                 .build();
 
@@ -55,10 +61,25 @@ public class TodoService {
     }
 
 
-
-
     // todo 수정
+    public TodoCreateResDto updateTodo(String todoId, UUID userId, UpdateTodoReqDto dto) {
+        // 1. 검증
+        var todo = sharedCalendarRepository.findById(todoId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 todo입니다."));
 
+        validationService.validateUserAndWorkspace(userId, todo.getWorkspaceId());
+
+        if (!todo.getUserId().getId().equals(userId))
+            throw new IllegalArgumentException("본인 todo만 수정할 수 있습니다.");
+
+        if (todo.getCalendarType() != CalendarType.TODO) {
+            throw new IllegalArgumentException("calendarType이 TODO인 일정만 수정할 수 있습니다.");
+        }
+
+        // 2. todo 수정
+        todo.updateTodo(dto.getCalendarName(), dto.getDate(), dto.getBookmark());
+        return TodoCreateResDto.fromEntity(todo);
+    }
 
     // todo 삭제
 
