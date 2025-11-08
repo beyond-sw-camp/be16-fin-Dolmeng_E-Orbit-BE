7086a019810849b4029ad05e158accb923a468fa
EunDuk2 <dmsqls6789@naver.com>
2025-11-08 16:48:41 +0900

    Chore: 직렬화를 위한 어노테이션 추가

--- workspace/src/main/java/com/Dolmeng_E/workspace/common/dto/SharedCalendarResDto.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/common/dto/SharedCalendarResDto.java
@@ -1,12 +1,16 @@
 package com.Dolmeng_E.workspace.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SharedCalendarResDto {
     private String id;
     private String userId;
