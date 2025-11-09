9d79336fa3b7f765f16b1d243d554b246b33acc6
jominhyeong97 <jumbo0303@daum.net>
2025-11-09 18:39:27 +0900

    Feat 담당자 dto 수정

--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/dto/StoneManagerModifyDto.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/dto/StoneManagerModifyDto.java
@@ -1,12 +1,16 @@
 package com.Dolmeng_E.workspace.domain.stone.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StoneManagerModifyDto {
     private String stoneId;
     private UUID newManagerUserId;
